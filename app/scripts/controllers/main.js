f39c33efc724609aac7e12ae998425fdcd71b71c
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2015-03-18 15:22:23 +0530

    Initial commit.
    Run 'yo angular' and commit the result.
    Add Bootstrap (without Sass and Compass) and the default AngularJS modules.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bankPlusApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bankPlusApp
+ */
+angular.module('bankPlusApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
