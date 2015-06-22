b0279979685402db1eb2cca49ca25ff34ea73190
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2015-06-22 23:47:36 +0530

    Updated Keycloak config.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -158,7 +158,7 @@ angular.element(document).ready(function ($http) {
   keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
     auth.loggedIn = true;
     auth.authz = keycloakAuth;
-    auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/bankplus/tokens/logout?redirect_uri=http://localhost:9000/";
+    auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/Bankplus/tokens/logout?redirect_uri=http://localhost:9000/";
 
     var initInjector = angular.injector(["ng"]);
     var $http = initInjector.get("$http");
