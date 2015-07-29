4cbe0556bc11cc634bec1e9e83ca4cf7289df2b8
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2015-07-29 16:12:03 +0530

    Fixed bug in looking up contacts when making payments

--- src/main/java/org/jboss/examples/bankplus/transactions/services/adapters/ContactsAdapter.java
+++ src/main/java/org/jboss/examples/bankplus/transactions/services/adapters/ContactsAdapter.java
@@ -2,6 +2,7 @@ package org.jboss.examples.bankplus.transactions.services.adapters;
 
 import org.jboss.examples.bankplus.customer.services.Contacts;
 import org.jboss.examples.bankplus.transactions.model.Contact;
+import org.jboss.examples.bankplus.transactions.services.translators.ContactTranslator;
 
 import javax.inject.Inject;
 
@@ -10,8 +11,11 @@ public class ContactsAdapter {
     @Inject
     private Contacts contacts;
 
+    @Inject
+    private ContactTranslator translator;
+
     public Contact findById(Long payeeId) {
-        contacts.findById(payeeId);
-        return null;
+        org.jboss.examples.bankplus.customer.model.Contact contact = contacts.findById(payeeId);
+        return translator.translate(contact);
     }
 }
--- src/main/java/org/jboss/examples/bankplus/transactions/services/translators/ContactTranslator.java
+++ src/main/java/org/jboss/examples/bankplus/transactions/services/translators/ContactTranslator.java
@@ -1,7 +1,18 @@
 package org.jboss.examples.bankplus.transactions.services.translators;
 
-/**
- * Created by vreynolds on 7/29/15.
- */
+import org.jboss.examples.bankplus.transactions.model.Contact;
+
 public class ContactTranslator {
+
+    public Contact translate(org.jboss.examples.bankplus.customer.model.Contact container) {
+        Contact contact = null;
+        if(container != null) {
+            contact = new Contact();
+            contact.setContactReference(Long.toString(container.getId()));
+            contact.setFullName(container.getFullName());
+            contact.setIban(container.getIban());
+        }
+        return contact;
+    }
+
 }
