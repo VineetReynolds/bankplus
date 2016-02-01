7cde69662c9cef7dd32c61277c6d8724e6d9c4b5
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2016-02-01 16:46:54 +0530

    Fixed referrer name for KeyCloak.
    Also fixed Docker compose instructions.

--- frontend/build-docker-image.sh
+++ frontend/build-docker-image.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Install grunt and other node dependencies
+npm install
+
+# Build the project
 grunt build
+
+# Build the docker container
 docker build -t bankplus-frontend .
--- start-cluster.sh
+++ start-cluster.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Disable dnsmasq because skydns will bind to port 53
+# sudo systemctl stop dnsmasq.service
+# sudo systemctl disable dnsmasq.service
 docker-compose -p bankplus up -d
