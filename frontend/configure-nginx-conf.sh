51eacf3fade82e7cc125504ad445c394d7e0a230
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2016-04-21 15:34:36 +0530

    Yet another workaround for nginx error log config.

--- frontend/configure-nginx-conf.sh
+++ frontend/configure-nginx-conf.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -eof pipefail
 
 if [ -z "$BANKPLUS_CUSTOMERS_SERVICE_HOST" ]; then
     echo "Using default nginx config"
@@ -8,6 +10,11 @@ else
     /usr/bin/sed -i "s/<customers_host_placeholder>/${BANKPLUS_CUSTOMERS_SERVICE_HOST}/" $HOME/nginx-conf/openshift.conf
     /usr/bin/sed -i "s/<transactions_host_placeholder>/${BANKPLUS_TRANSACTIONS_SERVICE_HOST}/" $HOME/nginx-conf/openshift.conf
     /usr/bin/sed -i "s/<reporting_host_placeholder>/${BANKPLUS_REPORTING_SERVICE_HOST}/" $HOME/nginx-conf/openshift.conf
+
+    # Employing workaround for Docker issue https://github.com/docker/docker/issues/6880. Credits to https://github.com/deis/router/pull/124
+    mkfifo -m 600 /tmp/logpipe
+    cat < /tmp/logpipe 1>&2 &
+
     exec /usr/sbin/nginx -c $HOME/nginx-conf/openshift.conf -g "daemon off;"
 fi
 
